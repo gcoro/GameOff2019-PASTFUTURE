883b4aabeabf2be235aae5750c40945c402f9370
Marco <pozza.marco95@gmail.com>
2019-11-14 22:38:37 +0100

    Simple movement script
    Create the first simple movement script. This is use to simulate the real movements that will be added later.

---
+++ Assets/Character/characterMovement.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    //Public references to character component
+
+    public Rigidbody movementDirection;
+    public float movementsForce = 0;
+    public float jumpForce = 0;
+
+    public Transform characterProperty;
+    public float characterHeight = 2;
+    public float characterWidth = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.initialization();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.defaultCharacterSize();
+        if(this.onEnterForwordKeys())
+            this.moveForward();
+        if(this.onEnterBackwordKeys())
+            this.moveBackword();
+        if(this.onEnterJumpKeys())
+            this.jump();
+        if(this.onEnterCrouchKeys())
+            this.crouch();
+    }
+
+    ////////////////////////////////////////////////////
+    //////////////////// UTILITY FUNCTION //////////////
+    ////////////////////////////////////////////////////
+
+    private void initialization(){
+        this.characterProperty.localScale = new Vector3(this.characterWidth,this.characterHeight,1);
+    }
+    //Move Forward
+    private bool onEnterForwordKeys(){
+        return Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow);
+    }
+    private void moveForward(){
+        this.movementDirection.AddForce(movementsForce * Time.deltaTime,0,0);
+    }
+
+    //Move Backword
+    private bool onEnterBackwordKeys(){
+        return Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow);
+    }
+    private void moveBackword(){
+        this.movementDirection.AddForce(-movementsForce * Time.deltaTime,0,0);
+    }
+
+    //Jump
+    private bool onEnterJumpKeys(){
+        return Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow);
+    }
+    private void jump(){
+        this.movementDirection.AddForce(0,jumpForce * Time.deltaTime,0);
+    }
+
+    //Crouch
+    private bool onEnterCrouchKeys(){
+        return Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow);
+    }
+    private void crouch(){
+        float crouchRatio = (float)Math.Round(this.characterHeight * 2) / 4;
+        this.characterProperty.localScale = new Vector3(this.characterWidth,this.characterHeight - crouchRatio ,1);
+    }
+    private void defaultCharacterSize(){
+        this.characterProperty.localScale = new Vector3(this.characterWidth,this.characterHeight,1);
+    }
+}
