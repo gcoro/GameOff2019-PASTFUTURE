ae0a54e59f6ac37da1eb6f9b6b1b8e1847156b1a
davide.trevisan@ennova-research.com <davide.trevisan@ennova-research.com>
2019-11-17 17:48:27 +0100

    tests on loop

---
+++ Assets/move_platform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_platform : MonoBehaviour
+{
+    public GameObject otherObj;
+    private bool lastState;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (lastState != gameObject.GetComponent<Renderer>().isVisible)
+        {
+            if (gameObject.GetComponent<Renderer>().isVisible)
+                otherObj.transform.position = transform.position + new Vector3(gameObject.GetComponent<Renderer>().bounds.size.x, 0, 0);
+           else if (!otherObj.GetComponent<Renderer>().isVisible)
+            {
+                otherObj.transform.position = transform.position + new Vector3(gameObject.GetComponent<Renderer>().bounds.size.x, 0, 0);
+            }
+        }
+    }
+}
---
+++ Assets/movimento.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento : MonoBehaviour
+{
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = transform.position + new Vector3(speed,0,0);//*time.deltatime
+    }
+}
