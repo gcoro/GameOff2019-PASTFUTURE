d509e442baae2b9110c12f8b8226f9dee2a13321
Marco <pozza.marco95@gmail.com>
2019-11-20 20:56:26 +0100

    Add camera follow script

---
+++ Assets/Camera/followPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform playerReference;
+    public Vector3 offset;    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerReference.position + offset;    
+    }
+}
