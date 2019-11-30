16f65282267c01c014148253bda7f5d104aa00d8
davide.trevisan@ennova-research.com <trexy94@live.it>
2019-11-30 16:23:25 +0100

    aggiunta schermata iniziale

---
+++ Assets/click_handler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class click_handler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        SceneManager.LoadScene("MainScene");
+    }
+}
