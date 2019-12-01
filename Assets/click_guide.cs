7ab6b5337dd30ac45e11bbf878e8cdfa21fb3a78
davide.trevisan@ennova-research.com <trexy94@live.it>
2019-12-01 17:37:17 +0100

    improvements in navigation

---
+++ Assets/click_guide.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class click_guide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        SceneManager.LoadScene("CommandScene");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("CommandScene");
+    }
+}
---
+++ Assets/return_start.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class return_start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
